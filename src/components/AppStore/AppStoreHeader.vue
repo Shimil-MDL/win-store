2a63a7d4cb65918d0615468d19a350dffa5c3cc6
Shimil-MDL <77040584+Shimil-MDL@users.noreply.github.com>
2021-07-03 00:31:13 +0530

    Update AppStoreHeader.vue

--- src/components/AppStore/AppStoreHeader.vue
+++ src/components/AppStore/AppStoreHeader.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="appStoreHeader">
-
     <div class="logo">
       <img src="../../assets/Microsoft_Store_Dark.svg" alt="Microsoft Store" />
       <div class="text">Microsoft Store</div>
@@ -18,31 +17,33 @@
     <div class="control-btns">
       <div class="btn" v-for="icon in ctrlBtn" :key="icon" v-html="icon"></div>
     </div>
-
   </div>
 </template>
 
 <script>
 export default {
   name: "AppStoreHeader",
-  data(){
-      return{
-          ctrlBtn: [`<i class="fas fa-minus"></i>`, `<i class="far fa-clone"></i>`, `<i class="fas fa-times"></i>`],
-      };
-  }
+  data() {
+    return {
+      ctrlBtn: [
+        `<i class="fas fa-minus"></i>`,
+        `<i class="far fa-clone"></i>`,
+        `<i class="fas fa-times"></i>`,
+      ],
+    };
+  },
 };
 </script>
 
 <style scoped>
-
 .appStoreHeader {
-
   position: absolute;
-  width: 98%;
+  width: 100%;
   height: 8%;
   border-radius: 10px;
-	background: var(--bg);
-	color: var(--fg);
+  border-bottom-left-radius: 0;
+  border-bottom-right-radius: 0;
+  color: var(--fg);
   display: flex;
   align-items: center;
   justify-content: space-around;
@@ -57,9 +58,9 @@ export default {
   width: 15%;
   margin-right: 40px;
   cursor: pointer;
-  -webkit-user-select: none; 
-  -ms-user-select: none; 
-  user-select: none;  
+  -webkit-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
 }
 .appStoreHeader .logo img {
   width: 1.5rem;
@@ -69,9 +70,8 @@ export default {
   font-weight: bold;
 }
 
-
 .appStoreHeader .search {
-	background: var(--inputs);
+  background: var(--inputs);
   box-shadow: var(--search-shadow);
   width: 50%;
   border-radius: 5px;
@@ -84,7 +84,7 @@ export default {
 }
 .appStoreHeader .search i {
   transform: rotate(90deg);
-	color: var(--fg);
+  color: var(--fg);
 }
 .appStoreHeader .search input {
   width: 90%;
@@ -92,19 +92,18 @@ export default {
   outline: none;
   border: none;
   font-size: 15px;
-	color: var(--fg);
+  color: var(--fg);
 }
 .appStoreHeader .search input[type="text"]:focus {
   font-size: 15px;
   background-color: transparent;
-	caret-color: var(--fg);
+  caret-color: var(--fg);
 }
 .appStoreHeader .search::placeholder {
   font-size: 15px;
-	color: var(--fg);
+  color: var(--fg);
   opacity: 0.5;
 }
-
 
 .appStoreHeader .profile {
   width: 45px;
@@ -112,7 +111,7 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
-  margin-right: -25px;
+  margin-right: 12%;
   cursor: pointer;
 }
 .appStoreHeader .profile i {
@@ -120,9 +119,11 @@ export default {
   color: var(--icons);
 }
 
-
 .appStoreHeader .control-btns {
   --ctrl-btns: rgb(0, 0, 0);
+  position: absolute;
+  right: 1%;
+  top: 0;
   margin: 0 -10px;
   width: 13%;
   height: 1.8rem;
@@ -137,7 +138,7 @@ export default {
 .appStoreHeader .control-btns .btn {
   height: 100%;
   width: calc(100% / 3.1);
-  border-radius: 3px;
+  border-radius: 1px;
 
   display: flex;
   align-items: center;
@@ -148,6 +149,9 @@ export default {
 }
 .appStoreHeader .control-btns .btn:hover {
   background: var(--btn-hover);
+}
+.appStoreHeader .control-btns .btn:nth-child(3) {
+  border-top-right-radius: 10px;
 }
 .appStoreHeader .control-btns .btn:nth-child(3):hover {
   background: var(--close-btn-hover);
