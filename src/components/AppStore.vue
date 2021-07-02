769d5e966aa1c651d8d8a6ac7235a8c1d3cae551
Shimil-MDL <77040584+Shimil-MDL@users.noreply.github.com>
2021-07-03 00:28:26 +0530

    Update AppStore.vue

--- src/components/AppStore.vue
+++ src/components/AppStore.vue
@@ -1,53 +1,46 @@
 <template>
   <div class="appStore">
-    
-    <AppStoreHeader />    
+    <AppStoreHeader />
     <AppStoreMenu />
     <AppStoreMain />
-
   </div>
 </template>
 
 <script>
-import AppStoreHeader from './AppStore/AppStoreHeader.vue'
-import AppStoreMenu from './AppStore/AppStoreMenu.vue'
-import AppStoreMain from './AppStore/AppStoreMain.vue'
-
+import AppStoreHeader from "./AppStore/AppStoreHeader.vue";
+import AppStoreMenu from "./AppStore/AppStoreMenu.vue";
+import AppStoreMain from "./AppStore/AppStoreMain.vue";
 
 export default {
-    name:"AppStore",
-    components:{
-      AppStoreHeader,
-      AppStoreMenu,
-      AppStoreMain
-    },
-}
+  name: "AppStore",
+  components: {
+    AppStoreHeader,
+    AppStoreMenu,
+    AppStoreMain,
+  },
+};
 </script>
 
 <style scoped>
-
-  .appStore{
-    /* variables */
+.appStore {
+  /* variables */
   --main-bg: hsl(300, 2%, 9%);
   --bg: #201f20;
-	--fg: #d6d6d6;
+  --fg: #d6d6d6;
 
-	--inputs: #222122;
-  --icons:  hsl(175, 70%, 33%);
+  --inputs: #111111;
+  --icons: hsl(175, 70%, 33%);
   --btn-hover: #72727259;
-  --close-btn-hover : #c21212a4;
+  --close-btn-hover: #c21212a4;
 
-  --search-shadow : 0px 0px 4px 1px #00000099;
+  --search-shadow: 0px 0px 4px 1px #00000099;
   --icons-active: #e4e4e4;
-    /* variables */
+  /* variables */
 
-    width: 70vw;
-    height: 85vh;
-    border-radius: 15px;
-    background: var(--main-bg);
-    padding: 10px;
-    position: relative;
-  }
-
-
+  width: 70vw;
+  height: 85vh;
+  border-radius: 15px;
+  background: var(--main-bg);
+  position: relative;
+}
 </style>
